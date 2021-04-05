829b44553358116bef8a43ae3c01f7d77b2556d1
Diego Pino Navarro <dpino@metro.org>
2021-04-04 20:38:08 -0400

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,32 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+    'use strict';
+
+    Drupal.behaviors.metro_subtheme = {
+        attach: function (context, settings) {
+            $(window).on('resize', function() {
+                // de-collapses if collapsed and media query restores large viewport.
+                var MOBILE_WIDTH = 992;
+                if ($(window).width() >= MOBILE_WIDTH) {
+                    $('.navbar-collapse.collapse.show').removeClass('show');
+                }
+            });
+
+            // Triggers admin toolbar offset on page load
+            if (context === document) {
+                $(context).on("drupalViewportOffsetChange.toolbar", function (event, offsets) {
+                    let $body = $("body");
+                    if ($body.length > 0 && offsets.top > 0 && $body.css('padding-top') !== offsets.top) {
+                        $body.css("padding-top", offsets.top);
+                    }
+                });
+            }
+        }
+    }
+
+})(jQuery, Drupal);
