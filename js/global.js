6ed03dcdeb52b33cf88a4ecbb367d8cc7e1e5e80
root <root@ip-172-30-0-33.ec2.internal>
2021-08-17 21:52:13 +0000

    Lastest Live Updates by August 2021 includes Mobile improvements
      Committer:  Diego Pino Navarro <DiegoPino@users.noreply.github.com>

--- js/global.js
+++ js/global.js
@@ -9,16 +9,32 @@
 
     Drupal.behaviors.metro_subtheme = {
         attach: function (context, settings) {
-            $(window).on('resize', function() {
-                // de-collapses if collapsed and media query restores large viewport.
+
+            function resize() {
                 var MOBILE_WIDTH = 992;
                 if ($(window).width() >= MOBILE_WIDTH) {
                     $('.navbar-collapse.collapse.show').removeClass('show');
+                    // Only move if not in its place already
+                    if ($("#navbar-top > #block-metro-subtheme-exposedformsolr-search-contentpage-1").length) {
+                       $('#navbar-top .views-exposed-form').insertAfter($('#block-mainnavigation-2'));
+                    }
                 }
+                if ($(window).width() <= MOBILE_WIDTH) {
+                    $('#navbar-top .views-exposed-form').insertAfter($('.navbar-toggler'));
+                }
+
+            }
+
+            $(window).on('resize', function() {
+                // de-collapses if collapsed and media query restores large viewport.
+                resize();
             });
 
             // Triggers admin toolbar offset on page load
             if (context === document) {
+                $(context).ready( function() {
+                  resize();
+                });
                 $(context).on("drupalViewportOffsetChange.toolbar", function (event, offsets) {
                     let $body = $("body");
                     if ($body.length > 0 && offsets.top > 0 && $body.css('padding-top') !== offsets.top) {
